a22864e07f59f51e742268a27a2643d17c594e57
yk-graph <yokokura.tatsuya@gmail.com>
2026-09-10 20:17:43 -0700

    chore: remove unused Geist fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,9 @@
-import type { Metadata } from 'next'
-import { Geist, Geist_Mono, Inter } from 'next/font/google'
-import './globals.css'
 import { cn } from '@/lib/utils'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -22,10 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html
-      lang="en"
-      className={cn('h-full', 'antialiased', geistSans.variable, geistMono.variable, 'font-sans', inter.variable)}
-    >
+    <html lang="en" className={cn('h-full', 'antialiased', 'font-sans', inter.variable)}>
       <body className="flex min-h-full flex-col">{children}</body>
     </html>
   )
