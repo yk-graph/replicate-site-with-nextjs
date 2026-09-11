d0df9bc8733a8aa9fefef71c29882d1615f5937b
yk-graph <yokokura.tatsuya@gmail.com>
2026-09-11 11:56:45 -0700

    add: compose layout and home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { FeedProvider } from '@/components/feed/feed-context'
+import { Nav } from '@/components/feed/nav'
+import { PhotoGrid } from '@/components/feed/photo-grid'
+import { Hero } from '@/components/sections/hero'
+import { TopCards } from '@/components/sections/top-cards'
+import { Subscribe } from '@/components/sections/subscribe'
+
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      replicate-site-with-nextjs
-    </div>
+    <FeedProvider>
+      <Nav />
+      <Hero />
+      <TopCards />
+      <PhotoGrid />
+      <Subscribe />
+    </FeedProvider>
   )
 }
